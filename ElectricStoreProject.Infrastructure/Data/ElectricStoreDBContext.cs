7d92cdeab3bb42fe5874a01a190c5437bf05902f
Vũ Quang Nguyên <nguyenvqse180208@fpt.edu.vn>
2026-01-10 17:41:50 +0700

    Add EF Core setup and initial DB context class
    Added ElectricStoreDBContext class (empty) in Infrastructure. Updated .gitignore for migration and env files. Added .env as a solution item. Added EF Core and related NuGet packages to Infrastructure project.

---
+++ ElectricStoreProject.Infrastructure/Data/ElectricStoreDBContext.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ElectricStoreProject.Infrastructure.Data
+{
+    public class ElectricStoreDBContext
+    {
+    }
+}
